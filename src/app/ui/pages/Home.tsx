d9b384e65ddb5c2fde17f0540af58a659579e102
Jessy <c.j91350@gmail.com>
2022-09-05 16:27:51 +0200

    Set up clean archi

---
+++ src/app/ui/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <h1>Battleship</h1>
+  )
+}
+
+export default HomePage
--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HomePage />
   </React.StrictMode>
 );
 
